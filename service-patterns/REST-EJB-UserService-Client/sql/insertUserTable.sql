816437ba29964b89bd5a3f09ced4ba4a125ab529
teiniker <egon.teiniker@gmail.com>
2021-02-22 09:03:36 +0100

    Moved examples from secure-design repo.

--- service-patterns/REST-EJB-UserService-Client/sql/insertUserTable.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO user (id, username, password) VALUES (1, 'homer', 'ijD8qepbRnIsva0kx0cKRCcYysg=');
-INSERT INTO user (id, username, password) VALUES (2, 'marge', 'xCSuPDv2U6I5jEO1wqvEQ/jPYhY=');
-INSERT INTO user (id, username, password) VALUES (3, 'bart', 'Ls4jKY8G2ftFdy/bHTgIaRjID0Q=');
-INSERT INTO user (id, username, password) VALUES (4, 'lisa', 'xO0U4gIN1F7bV7X7ovQN2TlSUF4=');
-
-
--- service-patterns/SOAP-EJB-Handler/README.txt
+++
@@ -1,6 +0,0 @@
-How to access the WSDL definition?
--------------------------------------------------------------------------------
-
-URL: http://localhost:8080/SOAP-EJB-Handler/UserService?wsdl
-
-
