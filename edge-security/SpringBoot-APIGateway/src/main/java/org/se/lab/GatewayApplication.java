7cf4d44e98494d33c3607ef753a7d975d42a5ec0
teiniker <egon.teiniker@gmail.com>
2021-11-05 11:21:23 +0100

    Added filtering.

--- edge-security/SpringBoot-APIGateway/src/main/java/org/se/lab/GatewayApplication.java
+++ edge-security/SpringBoot-APIGateway/src/main/java/org/se/lab/GatewayApplication.java
@@ -3,6 +3,7 @@ package org.se.lab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -11,5 +12,11 @@ public class GatewayApplication
     public static void main(String[] args)
     {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public LoggingFilter loggingFilter()
+    {
+        return new LoggingFilter();
     }
 }
