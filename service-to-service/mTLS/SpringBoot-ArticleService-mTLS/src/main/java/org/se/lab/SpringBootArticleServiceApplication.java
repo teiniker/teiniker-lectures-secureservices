7658c4d3f787415117d44bf23becde6cf4dec788
teiniker <egon.teiniker@gmail.com>
2021-11-25 19:20:27 +0100

    Refactoring.

--- service-to-service/mTLS/SpringBoot-ArticleService-mTLS/src/main/java/org/se/lab/SpringBootArticleServiceApplication.java
+++ service-to-service/mTLS/SpringBoot-ArticleService-mTLS/src/main/java/org/se/lab/SpringBootArticleServiceApplication.java
@@ -1,4 +1,4 @@
-package org.se.lab.service;
+package org.se.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,22 +6,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.File;
 
 @SpringBootApplication
-public class UserProcessingApp
+public class SpringBootArticleServiceApplication
 {
-	static {
-
+	static
+	{
 		String path = System.getProperty("user.dir");
-		String keystorePath = path + File.separator + "keystore.jks";
+		String keystorePath = path + File.separator + "src/main/resources/server.jks";
 		File file = new File(keystorePath);
+		System.out.println("Path (trustStore): " + file.getAbsolutePath());
 
 		if (file.exists()) {
 			System.setProperty("javax.net.ssl.trustStore", keystorePath);
-			System.setProperty("javax.net.ssl.trustStorePassword", "springboot");
+			System.setProperty("javax.net.ssl.trustStorePassword", "student");
 		}
 	}
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(UserProcessingApp.class, args);
+		SpringApplication.run(SpringBootArticleServiceApplication.class, args);
 	}
 }
