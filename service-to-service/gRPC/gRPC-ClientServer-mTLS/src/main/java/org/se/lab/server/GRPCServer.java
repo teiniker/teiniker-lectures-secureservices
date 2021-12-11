32b652f2084fd42d6b9466c996129f26cb89c9fa
teiniker <egon.teiniker@gmail.com>
2021-12-11 11:58:34 +0100

    Refactoring.

--- service-to-service/gRPC/gRPC-ClientServer-mTLS/src/main/java/org/se/lab/server/GRPCServer.java
+++ service-to-service/gRPC/gRPC-ClientServer-mTLS/src/main/java/org/se/lab/server/GRPCServer.java
@@ -4,6 +4,7 @@ import io.grpc.Server;
 import io.grpc.netty.GrpcSslContexts;
 import io.grpc.netty.NettyServerBuilder;
 import io.netty.handler.ssl.ClientAuth;
+import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslProvider;
 
@@ -12,84 +13,40 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.logging.Logger;
 
+import static io.grpc.netty.GrpcSslContexts.configure;
+
 public class GRPCServer
 {
     private static final Logger LOG = Logger.getLogger(GRPCServer.class.getName());
 
-    private Server server;
-
-    private final String host;
-    private final int port;
-    private final String certChainFilePath;
-    private final String privateKeyFilePath;
-    private final String trustCertCollectionFilePath;
-
-
-    public GRPCServer(String host, int port,
-                      String certChainFilePath, String privateKeyFilePath, String trustCertCollectionFilePath)
-    {
-        this.host = host;
-        this.port = port;
-        this.certChainFilePath = certChainFilePath;
-        this.privateKeyFilePath = privateKeyFilePath;
-        this.trustCertCollectionFilePath = trustCertCollectionFilePath;
-    }
-
     public static void main(String[] args) throws IOException, InterruptedException
     {
-        final GRPCServer server = new GRPCServer("localhost",50440,
-                "/tmp/sslcert/server.crt", "/tmp/sslcert/server.pem", "/tmp/sslcert/ca.crt");
-        server.start();
-        server.blockUntilShutdown();
-    }
+        SslContextBuilder sslClientContextBuilder = SslContextBuilder.forServer(new File("/tmp/sslcert/server.crt"),
+                new File("/tmp/sslcert/server.pem"))
+                .trustManager(new File("/tmp/sslcert/ca.crt"))
+                .clientAuth(ClientAuth.REQUIRE);
 
-    private SslContextBuilder getSslContextBuilder()
-    {
-        SslContextBuilder sslClientContextBuilder = SslContextBuilder.forServer(new File(certChainFilePath),
-                new File(privateKeyFilePath));
-        if (trustCertCollectionFilePath != null)
-        {
-            sslClientContextBuilder.trustManager(new File(trustCertCollectionFilePath));
-            sslClientContextBuilder.clientAuth(ClientAuth.REQUIRE);
-        }
-        return GrpcSslContexts.configure(sslClientContextBuilder, SslProvider.OPENSSL);
-    }
+        SslContext sslContext = GrpcSslContexts.configure(sslClientContextBuilder, SslProvider.OPENSSL).build();
 
-    private void start() throws IOException
-    {
-        server = NettyServerBuilder.forAddress(new InetSocketAddress(host, port))
+        Server server = NettyServerBuilder.forAddress(new InetSocketAddress("localhost",50440))
                 .addService(new HelloServiceImpl())
-                .sslContext(getSslContextBuilder().build())
+                .sslContext(sslContext)
                 .build()
                 .start();
-        LOG.info("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
+        LOG.info("Server started...");
+
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 // Use stderr here since the logger may have been reset by its JVM shutdown hook.
                 System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                GRPCServer.this.stop();
+                server.shutdown();
                 System.err.println("*** server shut down");
             }
         });
-    }
 
-    private void stop()
-    {
-        if (server != null)
-        {
-            server.shutdown();
-        }
-    }
-
-    /**
-     * Await termination on the main thread since the grpc library uses daemon threads.
-     */
-    private void blockUntilShutdown() throws InterruptedException
-    {
-        if (server != null)
-        {
-            server.awaitTermination();
-        }
+        server.awaitTermination();
     }
 }
