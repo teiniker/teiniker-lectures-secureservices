43cf793abe08031c477458b552e20b58eda4bdf1
Egon Teiniker <egon.teiniker@gmail.com>
2022-10-27 18:22:56 +0200

    Initial import.

---
+++ api-security-basics/api-authorization/SpringBoot-Book-Authorization-Exercise/src/main/java/org/se/lab/InfoController.java
@@ -0,0 +1,21 @@
+package org.se.lab;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@RestController
+public class InfoController
+{
+    @GetMapping("/infos")
+    String info()
+    {
+        return "Book API Information (Version 1.0)";
+    }
+}
