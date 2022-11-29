341599862ddfe0767b7e99016da59efcd949d592
Egon Teiniker <egon.teiniker@gmail.com>
2022-11-29 21:12:24 +0100

    Refactoring.

--- api-security-basics/api-specifications/SpringBoot-BeanValidation-Book/src/main/java/org/se/lab/BookController.java
+++ api-security-basics/api-specifications/SpringBoot-BeanValidation-Book/src/main/java/org/se/lab/BookController.java
@@ -3,10 +3,13 @@ package org.se.lab;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -47,5 +50,14 @@ public class BookController
         newBook.setId(id);
         table.put(id, newBook);
         return new ResponseEntity<Book>(newBook, HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValid(MethodArgumentNotValidException ex)
+    {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
     }
 }
